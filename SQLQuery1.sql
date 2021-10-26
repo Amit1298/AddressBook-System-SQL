a970bf43404c7737592adc9634abf5edd9855941
Amit <amitkr24031995@gmail.com>
2021-10-26 07:26:30 +0530

    Adding Addressbook table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,12 @@
 create database Address_Book_service;
 use Address_Book_Service;
+
+create table Address_Book1(
+first_name varchar(50),
+last_name varchar(50),
+addres varchar(50),
+city varchar(50),
+states varchar(50),
+zip int,
+phone_number int,
+email varchar(50));
